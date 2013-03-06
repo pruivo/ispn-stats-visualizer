97ab617e816ebdaac2eb45527a23b89f2a190156
Pedro Ruivo <pruivo@redhat.com>
2013-03-06 22:13:42 +0000

    add current protocol

--- index.php
+++ index.php
@@ -1,193 +1,252 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-   <script src="js/excanvas.js" type="text/javascript"></script>
-   <script src="js/excanvas.min.js" type="text/javascript"></script>
-   <script src="js/jquery.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.js" type="text/javascript"></script>
-   <!--script src="js/jquery.colorhelpers.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.categories.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.crosshair.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.fillbetween.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.image.js" type="text/javascript"></script>   
-   <script src="js/jquery.flot.navigate.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.pie.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.resize.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.selection.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.stack.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.symbol.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.threshold.js" type="text/javascript"></script>
-   <script src="js/jquery.flot.time.js" type="text/javascript"></script-->
-   <title>Workload Monitor [Real Time plots]</title>
-   <script type="text/javascript">
-   <?php
-      $folder = "files";
+    <script src="js/excanvas.js" type="text/javascript"></script>
+    <script src="js/excanvas.min.js" type="text/javascript"></script>
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.js" type="text/javascript"></script>
+    <!--script src="js/jquery.colorhelpers.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.categories.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.crosshair.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.fillbetween.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.image.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.navigate.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.pie.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.resize.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.selection.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.stack.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.symbol.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.threshold.js" type="text/javascript"></script>
+    <script src="js/jquery.flot.time.js" type="text/javascript"></script-->
+    <title>Workload Monitor [Real Time plots]</title>
+    <script type="text/javascript">
+        <?php
+        $folder = "files";
 
-      if (isset($_REQUEST['apps'])) {
-         $folder = $_REQUEST['apps'];
-      }   	
-      
-      echo 'var folder = "' . $folder . '";';
-      echo 'var folderArray = [';
-      $array = split(",", $folder);
-      echo '"'.$array[0].'"';      
-      for ($idx = 1; $idx < count($array); ++$idx) {
-         echo ',"'.$array[$idx].'"'; 
-      }
-      echo '];';
-      
-      if (isset($_REQUEST['smooth'])) {
-         echo 'var smoothAlpha = ' . $_REQUEST['smooth'] . ';';
-      } else {
-         echo 'var smoothAlpha = 1;';
-      }
-   ?>
-   
-   function smooth(oldValue, newValue, alpha) {
-   		if (oldValue == -1 || alpha >= 1) return newValue;
-   		if (alpha <= 0) return oldValue;
-   		return alpha * newValue + (1 - alpha) * oldValue;
-   }
-   </script>
+        if (isset($_REQUEST['apps'])) {
+            $folder = $_REQUEST['apps'];
+        }
+
+        echo 'var folder = "' . $folder . '";';
+        echo 'var folderArray = [';
+        $phpFolderArray = preg_split('/,/', $folder);
+        echo '"' . $phpFolderArray[0] . '"';
+        for ($idx = 1; $idx < count($phpFolderArray); ++$idx) {
+            echo ',"' . $phpFolderArray[$idx] . '"';
+        }
+        echo '];';
+
+        if (isset($_REQUEST['smooth'])) {
+            echo 'var smoothAlpha = ' . $_REQUEST['smooth'] . ';';
+        } else {
+            echo 'var smoothAlpha = 1;';
+        }
+        ?>
+
+        function smooth(oldValue, newValue, alpha) {
+            if (oldValue == -1 || alpha >= 1) return newValue;
+            if (alpha <= 0) return oldValue;
+            return alpha * newValue + (1 - alpha) * oldValue;
+        }
+    </script>
 </head>
 <body>
 <h1>Workload Monitor - Real Time plots</h1>
-<table style="border:1px solid black;">
-   <tr>
-      <td>Throughput (transaction per second)</td>
-      <td>Write Tx Throughput (transaction per second)</td>
-      <td>Read Tx Throughput (transaction per second)</td>
-   </tr>
-   <tr>
-      <td><div id="throughput" style="width:500px;height:300px"></div></td>
-      <td><div id="writeThroughput" style="width:500px;height:300px"></div></td>
-      <td><div id="readThroughput" style="width:500px;height:300px"></div></td>
-   </tr>      
-   <tr>
-      <td>Write Percentage</td>
-      <td>Commit Latency (microseconds)</td>
-      <td>Abort Rate (percentage)</td>
-   </tr>
-   <tr>
-      <td><div id="wrtPer" style="width:500px;height:300px"></div></td>
-      <td><div id="commitLatency" style="width:500px;height:300px"></div></td>
-      <td><div id="abortRate" style="width:500px;height:300px"></div></td>
-   </tr>
-   <tr>
-      <td>Cpu Usage (percentage)</td>
-      <td>Memory Usage (GigaByte)</td>
-      <td>Average Lock Hold Time (microseconds)</td>
-   </tr>
-   <tr>
-      <td><div id="cpu" style="width:500px;height:300px"></div></td>
-      <td><div id="memory" style="width:500px;height:300px"></div></td>
-      <td><div id="lockHold" style="width:500px;height:300px"></div></td>
-   </tr>
-   <tr>
-      <td>Average RO tx execution time (microseconds)</td>
-      <td>Average Wr tx execution time (microseconds)</td>
-      <td>Expected Write Percentage</td>
-   </tr>
-   <tr>
-      <td><div id="roExec" style="width:500px;height:300px"></div></td>
-      <td><div id="wrExec" style="width:500px;height:300px"></div></td>
-      <td><div id="expWrtPer" style="width:500px;height:300px"></div></td>
-   </tr>            
+
+<table>
+    <?php
+    $numberOfColumns = 4;
+    foreach ($phpFolderArray as &$f) {
+        echo '<tr><td>Current protocol for <b>' . $f . '</b> is <span id="' . $f . '_protocol">N/A</span><td></tr>';
+    }
+    ?>
 </table>
 
-  <table>
-  </table>
-   
-<p>Time between updates:
-   <input id="updateInterval" type="text" value="" style="text-align: right; width:5em">
-   milliseconds</p>
+<table style="border:1px solid black;">
+    <tr>
+        <td>Throughput (transaction per second)</td>
+        <td>Write Tx Throughput (transaction per second)</td>
+        <td>Read Tx Throughput (transaction per second)</td>
+    </tr>
+    <tr>
+        <td>
+            <div id="throughput" style="width:500px;height:300px"></div>
+        </td>
+        <td>
+            <div id="writeThroughput" style="width:500px;height:300px"></div>
+        </td>
+        <td>
+            <div id="readThroughput" style="width:500px;height:300px"></div>
+        </td>
+    </tr>
+    <tr>
+        <td>Write Percentage</td>
+        <td>Commit Latency (microseconds)</td>
+        <td>Abort Rate (percentage)</td>
+    </tr>
+    <tr>
+        <td>
+            <div id="wrtPer" style="width:500px;height:300px"></div>
+        </td>
+        <td>
+            <div id="commitLatency" style="width:500px;height:300px"></div>
+        </td>
+        <td>
+            <div id="abortRate" style="width:500px;height:300px"></div>
+        </td>
+    </tr>
+    <tr>
+        <td>Cpu Usage (percentage)</td>
+        <td>Memory Usage (GigaByte)</td>
+        <td>Average Lock Hold Time (microseconds)</td>
+    </tr>
+    <tr>
+        <td>
+            <div id="cpu" style="width:500px;height:300px"></div>
+        </td>
+        <td>
+            <div id="memory" style="width:500px;height:300px"></div>
+        </td>
+        <td>
+            <div id="lockHold" style="width:500px;height:300px"></div>
+        </td>
+    </tr>
+    <tr>
+        <td>Average RO tx execution time (microseconds)</td>
+        <td>Average Wr tx execution time (microseconds)</td>
+        <td>Expected Write Percentage</td>
+    </tr>
+    <tr>
+        <td>
+            <div id="roExec" style="width:500px;height:300px"></div>
+        </td>
+        <td>
+            <div id="wrExec" style="width:500px;height:300px"></div>
+        </td>
+        <td>
+            <div id="expWrtPer" style="width:500px;height:300px"></div>
+        </td>
+    </tr>
+</table>
+
+<p>Time between updates: <input id="updateInterval" type="text" value="" style="text-align: right; width:5em"> milliseconds</p>
 
 <script type="text/javascript">
-   $(function () {
-      // setup control widget
-      var updateInterval = 1000;
-      
-      $("#updateInterval").val(updateInterval).change(function () {
-         var v = $(this).val();
-         if (v && !isNaN(+v)) {
-            updateInterval = +v;
-         if (updateInterval < 500)
-            updateInterval = 500;
-         if (updateInterval > 20000)
-            updateInterval = 20000;
-         $(this).val("" + updateInterval);
-         }
-      });
-      
-      // setup plot
-      var default_options = {
-         series: { shadowSize: 0 }, // drawing is faster without shadows
-         yaxis: { min: 0 },
-         xaxis: { min: 0 }
-      };   
-          
-      var log_options = {
-         series: { shadowSize: 0 }, // drawing is faster without shadows
-         yaxis: { transform: function (v) { if (v == 0) return 0; return Math.log(v); }, 
-                  inverseTransform: function (v) { if (v == 0) return 0; return Math.exp(v);}, 
-                  ticks: function logTickGenerator(axis) {
-                        var res = [], v = 100;                  
-                        do {
-                         v = v * 10;
-                         res.push(v);                   
-                        } while (v < axis.max);
-                        
-                        return res;
-                  }},
-         xaxis: { min: 0 }
-      };
-      
-      function updatePlot(div, param, avg, options) {
-         $.ajax({
-         url: "get-data.php?param=" + param + "&avg=" + avg + "&folder=" + folder,
-         method: 'GET',
-         dataType: 'text',
-         success: function(text) {                     
-            var lines = text.split("\n");            
-            var allData = [];
-            var dataIdx = 0;
-            var oldValue = -1;
-            var dataObj = { data: [], color: dataIdx, label: folderArray[dataIdx++]}            
-            for(var i = 0, j = 0; i < lines.length; i++) {
-               if (lines[i] == ".") {
-                  allData.push(dataObj);               
-                  dataObj = { data: [] , color: dataIdx, label: folderArray[dataIdx++]};                  
-                  j = 0;
-                  continue;
-               }
-               var keyValue = lines[i].split("|");
-               if (keyValue[0] == "" || keyValue[1] == "") continue;
-               oldValue = smooth(oldValue, keyValue[1], smoothAlpha);
-               dataObj.data[j++] = new Array(keyValue[0],oldValue);
-            }            
-            $.plot($("#" + div), allData, options);
-         }
-         });
-      }
-      
-      function update() {
-         updatePlot("throughput", "throughput", "false", default_options);
-         updatePlot("writeThroughput", "writeTxThroughput", "false", default_options);
-         updatePlot("readThroughput", "readTxThroughput", "false", default_options);
-         updatePlot("abortRate", "abortRate", "true", default_options);
-         updatePlot("cpu", "CPU", "true", default_options);
-         updatePlot("memory", "Memory.Usage", "true", default_options);
-         updatePlot("wrtPer", "percentageWriteTransactions", "true", default_options);
-         updatePlot("commitLatency", "CommitLatency", "true", log_options);
-         updatePlot("lockHold", "avgLockHoldTime", "true", default_options);
-         updatePlot("roExec", "avgReadOnlyTxDuration", "true", default_options);
-         updatePlot("wrExec", "avgWriteTxDuration", "true", default_options);
-         updatePlot("expWrtPer", "ExpectedWritePercentage", "true", default_options);
-         setTimeout(update, updateInterval);
-      }
-      
-      update();
-   });
+    $(function () {
+        // setup control widget
+        var updateInterval = 10000;
+
+        $("#updateInterval").val(updateInterval).change(function () {
+            var v = $(this).val();
+            if (v && !isNaN(+v)) {
+                updateInterval = +v;
+                if (updateInterval < 500)
+                    updateInterval = 500;
+                if (updateInterval > 20000)
+                    updateInterval = 20000;
+                $(this).val("" + updateInterval);
+            }
+        });
+
+        // setup plot
+        var default_options = {
+            series:{ shadowSize:0 }, // drawing is faster without shadows
+            yaxis:{ min:0 },
+            xaxis:{ min:0 }
+        };
+
+        var log_options = {
+            series:{ shadowSize:0 }, // drawing is faster without shadows
+            yaxis:{ transform:function (v) {
+                if (v == 0) return 0;
+                return Math.log(v);
+            },
+                inverseTransform:function (v) {
+                    if (v == 0) return 0;
+                    return Math.exp(v);
+                },
+                ticks:function logTickGenerator(axis) {
+                    var res = [], v = 100;
+                    do {
+                        v = v * 10;
+                        res.push(v);
+                    } while (v < axis.max);
+
+                    return res;
+                }},
+            xaxis:{ min:0 }
+        };
+
+        function updatePlot(div, param, avg, options) {
+            $.ajax({
+                url:"get-data.php?param=" + param + "&avg=" + avg + "&folder=" + folder,
+                method:'GET',
+                dataType:'text',
+                success:function (text) {
+                    var lines = text.split("\n");
+                    var allData = [];
+                    var dataIdx = 0;
+                    var oldValue = -1;
+                    var dataObj = { data:[], color:dataIdx, label:folderArray[dataIdx++]}
+                    for (var i = 0, j = 0; i < lines.length; i++) {
+                        if (lines[i] == ".") {
+                            allData.push(dataObj);
+                            dataObj = { data:[], color:dataIdx, label:folderArray[dataIdx++]};
+                            j = 0;
+                            continue;
+                        }
+                        var keyValue = lines[i].split("|");
+                        if (keyValue[0] == "" || keyValue[1] == "") continue;
+                        oldValue = smooth(oldValue, keyValue[1], smoothAlpha);
+                        dataObj.data[j++] = new Array(keyValue[0], oldValue);
+                    }
+                    $.plot($("#" + div), allData, options);
+                }
+            });
+        }
+
+        function updateProtocol() {
+            $.ajax({
+                url:"get-data.php?param=protocol&folder=" + folder,
+                method:'GET',
+                dataType:'text',
+                success:function (text) {
+                    var lines = text.split("\n");
+                    var lastValue = "N/A";
+                    var folderIndex = 0;
+                    for (var i = 0, j = 0; i < lines.length; i++) {
+                        if (lines[i] == ".") {
+                            $("#" + folderArray[folderIndex++] + "_protocol").html(lastValue);
+                            lastValue = "N/A";
+                            continue;
+                        }
+                        var keyValue = lines[i].split("|");
+                        if (keyValue[0] == "" || keyValue[1] == "") continue;
+                        lastValue = keyValue[1];
+                    }
+                }
+            });
+        }
+
+        function update() {
+            updateProtocol();
+            updatePlot("throughput", "throughput", "false", default_options);
+            updatePlot("writeThroughput", "writeTxThroughput", "false", default_options);
+            updatePlot("readThroughput", "readTxThroughput", "false", default_options);
+            updatePlot("abortRate", "abortRate", "true", default_options);
+            updatePlot("cpu", "CPU", "true", default_options);
+            updatePlot("memory", "Memory.Usage", "true", default_options);
+            updatePlot("wrtPer", "percentageWriteTransactions", "true", default_options);
+            updatePlot("commitLatency", "CommitLatency", "true", log_options);
+            updatePlot("lockHold", "avgLockHoldTime", "true", default_options);
+            updatePlot("roExec", "avgReadOnlyTxDuration", "true", default_options);
+            updatePlot("wrExec", "avgWriteTxDuration", "true", default_options);
+            updatePlot("expWrtPer", "ExpectedWritePercentage", "true", default_options);
+            setTimeout(update, updateInterval);
+        }
+
+        update();
+    });
 </script>
 </body>
 </html>
